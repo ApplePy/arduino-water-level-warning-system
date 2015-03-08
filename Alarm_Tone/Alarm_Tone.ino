79724b2b6fc38729135fbd707af4ada077c300fb
Darryl <darrylmurray@me.com>
2015-03-08 19:19:23 -0400

    Set up MAX_COUNT to automatically update itself.
    Moved speaker functionality to its own function.
    
    Set up sensor pin to use the internal pull-up resistor (save some money!)
    
    Re-wrote note timing so that all notes have the same(ish) duration.

--- Alarm_Tone/Alarm_Tone.ino
+++ Alarm_Tone/Alarm_Tone.ino
@@ -11,52 +11,65 @@
  *
  * where the different tones are described as in the table:
  *
- * note         frequency       period  PW (timeHigh)   
- * c            261 Hz          3830    1915    
- * d            294 Hz          3400    1700    
- * e            329 Hz          3038    1519    
- * f            349 Hz          2864    1432    
- * g            392 Hz          2550    1275    
- * a            440 Hz          2272    1136    
- * b            493 Hz          2028    1014    
+ * note         frequency       period  PW (timeHigh)
+ * c            261 Hz          3830    1915
+ * d            294 Hz          3400    1700
+ * e            329 Hz          3038    1519
+ * f            349 Hz          2864    1432
+ * g            392 Hz          2550    1275
+ * a            440 Hz          2272    1136
+ * b            493 Hz          2028    1014
  * C            523 Hz          1912    956
  *
  * (cleft) 2005 D. Cuartielles for K3
  */
 
 int ledPin = 13;
-int speakerOut = 9;               
-byte names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C'};  
+int speakerOut = 9;
+int sensorPin = 5;
+byte names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C'};
 int tones[] = {1915, 1700, 1519, 1432, 1275, 1136, 1014, 956};
-byte melody[] = "9b6d9b6d9b6d9b6d";
+byte melody[] = "3b3d"; //"3b3d3b3d3b3d3b3d";
 // count length: 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0
 //                                10                  20                  30
 int count = 0; //Iterates through the notes in the playlist
 int count2 = 0; //The counter that iterates through the names, looking for a match with the requested note
-int count3 = 0; //Controls how long a "beat" is, via its for loop
-int MAX_COUNT = 8; //How many notes are in the melody
+int MAX_COUNT = (sizeof(melody) - 1) / 2; //How many notes are in the melody
 int statePin = LOW;
+void speaker ();
 
 void setup() {
- pinMode(ledPin, OUTPUT);
- //Speaker is already set up by default as output
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+  //Speaker is already set up by default as output
 }
 
 void loop() {
-  analogWrite(speakerOut, 0); //Ensure speaker starts off 
+  if (digitalRead (sensorPin) == LOW) {
+    speaker();
+  }
+}
+
+void speaker() {
+  analogWrite(speakerOut, 0); //Ensure speaker starts off
   for (count = 0; count < MAX_COUNT; count++) { //Iterates through the notes requested
     statePin = !statePin;
     digitalWrite(ledPin, statePin);
-    for (count3 = 0; count3 <= (melody[count*2] - 48) * 30; count3++) { //controls how long a note plays
-      for (count2=0;count2<8;count2++) { //Iterate through tones
-        if (names[count2] == melody[count*2 + 1]) { //if you find a tone that matches the tone requested       
-          analogWrite(speakerOut,255); //pulse speaker accordingly (does not control volume well)
+    for (count2 = 0; count2 < 8; count2++) { //Iterate through tones
+      if (names[count2] == melody[count * 2 + 1]) { //if you find a tone that matches the tone requested
+        unsigned long startTime = millis();
+        while (millis() - startTime <= (melody[count * 2] - 48) * 100) { //rudimentary way of spacing out the length of a tone, and keeps tones at the same length
+          analogWrite(speakerOut, 255); //pulse speaker accordingly (does not control volume well)
           delayMicroseconds(tones[count2]);
           analogWrite(speakerOut, 0);
           delayMicroseconds(tones[count2]); //there is a timing issue here where higher notes are played slightly shorter
-        } 
-        if (melody[count*2 + 1] == 'p') {
-          // make a pause of a certain size
+        }
+      }
+      if (melody[count * 2 + 1] == 'p') {
+        // make a pause of a certain size
+        unsigned long startTime = millis();
+        while (millis() - startTime <= (melody[count * 2] - 48) * 30) {
           analogWrite(speakerOut, 0);
           delayMicroseconds(500);
         }
@@ -65,4 +78,4 @@ void loop() {
   }
 }
 
-//Code from http://www.arduino.cc/en/Tutorial/PlayMelody
+//Base code from http://www.arduino.cc/en/Tutorial/PlayMelody
